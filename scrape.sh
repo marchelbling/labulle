73f5032a7b5fb237038a7c011ea19ab5dadff46b
Marc Helbling <marc.d.helbling@gmail.com>
2018-09-21 12:25:30 +0200

    fix scrape

--- scrape.sh
+++ scrape.sh
@@ -14,6 +14,8 @@ trap terminate_screen EXIT
 
 
 function scrape {
+    pip3 install -r requirements.txt
+
     # lowercase
     local scraper="$( echo "${1}" | tr '[:upper:]' '[:lower:]' )"
     local jl="data/${scraper}.jl"
@@ -22,7 +24,6 @@ function scrape {
     scrapy crawl ${scraper} -o "${jl}" >data/${scraper}.log 2>&1
 
     # build & upload diff
-    pip3 install -r requirements.txt
     ./algdiff.py --app "IQKQPU4IQQ" --index "labulle" --key "${API_KEY}" --data "${jl}" >data/${scraper}.diff 2>data/${scraper}.err
 
     # commit data
